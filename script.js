3790033107d433bf86b08a3e4498f226bb7994bc
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-23 20:36:39 -0700

    finish render setup

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let lastRenderTime = 0;
 
+// How many times snake moves per second
+const SNAKE_SPEED = 2;
+
 function main(currentTime) {
     window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    // If snake needs to move
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
     lastRenderTime = currentTime;
-    console.log(currentTime);
+    // console.log(currentTime);
 }
 
 window.requestAnimationFrame(main);
