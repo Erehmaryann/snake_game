646c7ff1c220be1cf5858f83aa13e2c63a6856ad
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-30 21:10:29 -0700

    updated snake.js and script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { SNAKE_SPEED } from './snake';
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake';
 
 let lastRenderTime = 0;
 
@@ -8,7 +8,6 @@ function main(currentTime) {
     // If snake needs to move
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
     lastRenderTime = currentTime;
-    // console.log(currentTime);
     // Game logic
     update();
     // Draws everything on the screen
@@ -18,9 +17,9 @@ function main(currentTime) {
 window.requestAnimationFrame(main);
 
 function update() {
-
+    updateSnake();
 }
 
 function draw() {
-
+    drawSnake();
 }
--- snake.js
+++ snake.js
@@ -1,2 +1,8 @@
 // How many times snake moves per second
 export const SNAKE_SPEED = 1;
+
+export function update() {
+
+}
+
+export function draw() {}
