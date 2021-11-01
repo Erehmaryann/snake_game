25dbe281c29ec4536b7c11a181c9c6f7be925485
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-31 19:15:38 -0700

    start draw func functionality

--- snake.js
+++ snake.js
@@ -1,8 +1,14 @@
 // How many times snake moves per second
 export const SNAKE_SPEED = 1;
+// An array of x, y positions
+const snakeBody = [{ x: 11, y: 11 }];
 
 export function update() {
 
 }
 
-export function draw() {}
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div');
+    });
+}
