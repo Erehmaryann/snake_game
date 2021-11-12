4c04e514edb9e3f82bf43c1240d89b2fb62301b7
Ereh Maryann <edwardmaryann09@gmail.com>
2021-11-12 11:52:09 -0800

    populated the update func

--- snake.js
+++ snake.js
@@ -1,7 +1,11 @@
 // How many times snake moves per second
 export const SNAKE_SPEED = 1;
 // An array of x, y positions
-const snakeBody = [{ x: 11, y: 11 }];
+const snakeBody = [
+    { x: 10, y: 11 },
+    { x: 11, y: 11 },
+    { x: 12, y: 11 },
+];
 
 export function update() {
 
