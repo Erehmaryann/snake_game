24b2b1834a51f1783a536654a7eb242e038cabbd
Ereh Maryann <edwardmaryann09@gmail.com>
2021-11-22 21:09:45 -0800

    dynamically setup game direction

--- input.js
+++ input.js
@@ -1,5 +1,25 @@
 let inputDirection = { x: 0, y: 0 };
 
+window.addEventListener('keydown', e => {
+    switch (e.key) {
+        case 'ArrowUp':
+            inputDirection = { x: 0, y: -1 };
+            break;
+        case 'ArrowDown':
+            inputDirection = { x: 0, y: 1 };
+            break;
+        case 'ArrowLeft':
+            inputDirection = { x: -1, y: 0 };
+            break;
+        case 'ArrowRight':
+            inputDirection = { x: 1, y: 0 };
+            break;
+        default:
+            inputDirection = { x: 0, y: 0 };
+
+    }
+});
+
 export function getInputDirection() {
     return inputDirection;
 }
