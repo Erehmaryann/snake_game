2a3216b5f887252cd7e896f0d5d54b2c0dc8dc9c
Ereh Maryann <edwardmaryann09@gmail.com>
2021-12-05 19:54:33 -0800

    reassign seg

--- snake.js
+++ snake.js
@@ -9,12 +9,6 @@ const snakeBody = [
 ];
 let newSegments = 0;
 
-function addSegments() {
-    for (let i = 0; i < newSegments; i++) {
-        snakeBody[snakeBody.length];
-    }
-}
-
 export function update() {
     addSegments();
     const inputDirection = getInputDirection();
@@ -40,10 +34,6 @@ export function expandSnake(amount) {
     newSegments = amount;
 }
 
-function equalPositions(pos1, pos2) {
-    return;
-    pos1.x === pos2.x && pos1.y === pos2.y;
-}
 
 export function onSnake(position) {
     return snakeBody.some((segment) => {
@@ -51,3 +41,15 @@ export function onSnake(position) {
     });
 }
 
+function equalPositions(pos1, pos2) {
+    return;
+    pos1.x === pos2.x && pos1.y === pos2.y;
+}
+
+function addSegments() {
+    for (let i = 0; i < newSegments; i++) {
+        snakeBody.push({ ...snakeBody[snakeBody.length - 1] });
+    }
+
+    newSegments = 0;
+}
